ca5ac3e347abe42fae7d8d2902c70dbce338bcbe
lvapeab <lvapeab@gmail.com>
2019-03-01 10:37:35 +0100

    Add training options

--- video_captioner/training.py
+++ video_captioner/training.py
@@ -72,14 +72,49 @@ def train_model(params):
     total_start_time = timer()
 
     logger.debug('Starting training!')
-    training_params = {'n_epochs': params['MAX_EPOCH'], 'batch_size': params['BATCH_SIZE'],
-                       'homogeneous_batches': params['HOMOGENEOUS_BATCHES'], 'maxlen': params['MAX_OUTPUT_TEXT_LEN'],
-                       'lr_decay': params['LR_DECAY'], 'lr_gamma': params['LR_GAMMA'],
-                       'epochs_for_save': params['EPOCHS_FOR_SAVE'], 'verbose': params['VERBOSE'],
-                       'eval_on_sets': params['EVAL_ON_SETS_KERAS'], 'n_parallel_loaders': params['PARALLEL_LOADERS'],
-                       'extra_callbacks': callbacks, 'reload_epoch': params['RELOAD'], 'epoch_offset': params['RELOAD'],
+    training_params = {'n_epochs': params['MAX_EPOCH'],
+                       'batch_size': params['BATCH_SIZE'],
+                       'homogeneous_batches': params['HOMOGENEOUS_BATCHES'],
+                       'maxlen': params['MAX_OUTPUT_TEXT_LEN'],
+                       'joint_batches': params['JOINT_BATCHES'],
+                       'lr_decay': params.get('LR_DECAY', None),  # LR decay parameters
+                       'initial_lr': params.get('LR', 1.0),
+                       'reduce_each_epochs': params.get('LR_REDUCE_EACH_EPOCHS', True),
+                       'start_reduction_on_epoch': params.get('LR_START_REDUCTION_ON_EPOCH', 0),
+                       'lr_gamma': params.get('LR_GAMMA', 0.9),
+                       'lr_reducer_type': params.get('LR_REDUCER_TYPE', 'linear'),
+                       'lr_reducer_exp_base': params.get('LR_REDUCER_EXP_BASE', 0),
+                       'lr_half_life': params.get('LR_HALF_LIFE', 50000),
+                       'lr_warmup_exp': params.get('WARMUP_EXP', -1.5),
+                       'min_lr': params.get('MIN_LR', 1e-9),
+                       'epochs_for_save': params['EPOCHS_FOR_SAVE'],
+                       'verbose': params['VERBOSE'],
+                       'eval_on_sets': params['EVAL_ON_SETS_KERAS'],
+                       'n_parallel_loaders': params['PARALLEL_LOADERS'],
+                       'extra_callbacks': callbacks,
+                       'reload_epoch': params['RELOAD'],
+                       'epoch_offset': params.get('EPOCH_OFFSET', 0),
                        'data_augmentation': params['DATA_AUGMENTATION'],
-                       'patience': params.get('PATIENCE', 0), 'metric_check': params.get('STOP_METRIC', None)}
+                       'patience': params.get('PATIENCE', 0),  # early stopping parameters
+                       'metric_check': params.get('STOP_METRIC', None) if params.get('EARLY_STOP', False) else None,
+                       'eval_on_epochs': params.get('EVAL_EACH_EPOCHS', True),
+                       'each_n_epochs': params.get('EVAL_EACH', 1),
+                       'start_eval_on_epoch': params.get('START_EVAL_ON_EPOCH', 0),
+                       'tensorboard': params.get('TENSORBOARD', False),
+                       'n_gpus': params.get('N_GPUS', 1),
+                       'tensorboard_params': {'log_dir': params.get('LOG_DIR', 'tensorboard_logs'),
+                                              'histogram_freq': params.get('HISTOGRAM_FREQ', 0),
+                                              'batch_size': params.get('TENSORBOARD_BATCH_SIZE', params['BATCH_SIZE']),
+                                              'write_graph': params.get('WRITE_GRAPH', True),
+                                              'write_grads': params.get('WRITE_GRADS', False),
+                                              'write_images': params.get('WRITE_IMAGES', False),
+                                              'embeddings_freq': params.get('EMBEDDINGS_FREQ', 0),
+                                              'embeddings_layer_names': params.get('EMBEDDINGS_LAYER_NAMES', None),
+                                              'embeddings_metadata': params.get('EMBEDDINGS_METADATA', None),
+                                              'label_word_embeddings_with_vocab': params.get('LABEL_WORD_EMBEDDINGS_WITH_VOCAB', False),
+                                              'word_embeddings_labels': params.get('WORD_EMBEDDINGS_LABELS', None),
+                                              }
+                       }
     video_model.trainNet(dataset, training_params)
 
     total_end_time = timer()
